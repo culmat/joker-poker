39567f31f01e87ece3763ff2e9abd85b2cbf036d
Matthias Cullmann <matthias.cullmann@gmail.com>
2020-02-09 18:53:14 +0100

    session handling OK

--- joker-poker.js
+++ joker-poker.js
@@ -7,22 +7,33 @@ var app = new Vue({
     sessionIdInput : '',
     dialog : false,
     sessionId : null,
+    session : { name : 'Joker Poker', time : 0},
     connected : false,
     lastMessage : '',
-    clusterState : null,
+    clusterState : {"leader" : "?", "nodes" : []},
     isLeader : false,
     messageInput : '',
     yaiID : yai.createdAt,
-    pages : ['Home' , 'Cluster'],
-    page : "Home",
+    pages : [
+      ['Team' , 'fas fa-user-friends'] ,
+      ['Me' , 'fas fa-user'],
+      ['Settings' , 'fas fa-cog'],
+    ],
+    page : "Team",
+    pageIcon : 'fas fa-user-friends',
   },
   methods: {
     join: function (create) {
       this.sessionId = yai.setSessionId(create ? yai.uuid() : this.sessionIdInput);
       yai.connect();
     },
-    navigate: function (e) {
-      this.page = e ? e.target.innerText : this.page;
+    sessionChange: function () {
+      this.session.time = new Date().getTime();
+      yai.send({session : this.session});
+    },
+    navigate: function (p,i) {
+      this.page = p || this.page;
+      this.pageIcon = i || this.pageIcon;
       window.history.pushState({page : this.page, sessionId: this.sessionId  }, null, baseHref+this.sessionId +"/"+ this.page);
     },
     syncState: function (create) {
@@ -35,37 +46,55 @@ var app = new Vue({
   }
 });
 
+function loadLocalData(){
+  var data = JSON.parse(localStorage.getItem(app.sessionId));
+	if(data && data.session) app.session = data.session;
+}
+
+window.onunload  = function(){
+  if(!app.sessionId) return;
+	localStorage.setItem(app.sessionId, JSON.stringify({session : app.session}));
+};
 
 (function(){
   var path = document.URL.replace(baseHref, "");
   if (path) {
     var tokens =  path.split("/");
     app.sessionId = tokens[0];
-    app.page = tokens[1] || "Home";
+    app.page = tokens[1] || "Team";
+    app.pageIcon = app.pages.find(e => e[0] == app.page)[1]
   }
-  if(!app.sessionId)
+  if(!app.sessionId){
     app.dialog = true;
-  else
+  }  else {
+    loadLocalData();
     app.navigate();
+  }
 })();
 
 
 yai.getSessionId = function() {return app.sessionId;};
 yai.setSessionId = function (id) {
     app.sessionId = id;
+    loadLocalData();
     app.navigate();
 		return id;
 };
 
 yai .addListener("clusterChange" , app.syncState)
     .addListener("onboard" , function(yai) {
-      yai.send(app.lastMessage);
+      yai.send({session : app.session});
     })
     .addListener("connect" , function() {
       app.connected = true;
+      yai.send({session : app.session});
     })
     .addListener("message" , function(data) {
-      app.lastMessage = data;
+      if(data.session && data.session.time > app.session.time) {
+        app.session = data.session;
+      } else {
+        app.lastMessage = data;
+      }
     })
     .addListener("disconnect" , function(yai) {
       app.connected = false;
